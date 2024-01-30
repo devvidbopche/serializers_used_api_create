52c5c078ea59fa1632b378cce23ba907a5d899ec
devvidbopche <devidbopche9@gmail.com>
2024-01-30 23:40:05 +0530

    change the api  data formate

--- crudproject1/settings.py
+++ crudproject1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'enroll',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#data formate  json  formate 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+       'rest_framework.renderers.JSONRenderer',
+
+    ]
+}
--- enroll/api/views.py
+++ enroll/api/views.py
@@ -1,8 +1,12 @@
 from enroll.models import User
 from enroll.api.serializer import UserViewSet
-
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework. permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset =User.objects.all()
     serializer_class = UserViewSet
+
+authentication_classes = [SessionAuthentication,]
+permission_classes = [IsAuthenticated]
